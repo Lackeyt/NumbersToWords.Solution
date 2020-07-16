78919b78910238e70758fe923ea015a06aa5f3dc
JohnNils Olson <johnnils@gmail.com>
2020-07-16 13:56:38 -0700

    add code to pass single digit conversion test
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- NumbersToWords/Models/NumbersToWords.cs
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace NumbersToWords.Models
 {
   public class NumberToConvert
   {
+    private Dictionary<int, string> _digitStringifier = new Dictionary<int, string>() { {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}};
     public int Input { get; set; }
     public string StringNumber { get; set;}
     public NumberToConvert(int input)
@@ -11,7 +14,14 @@ namespace NumbersToWords.Models
     }
     public string ConvertNumber()
     {
+      string digitString = ConvertDigits(Input);
+      StringNumber = digitString + StringNumber;
       return StringNumber;
+    }
+
+    private string ConvertDigits(int input)
+    {
+      return _digitStringifier[input];
     }
   }
 }
