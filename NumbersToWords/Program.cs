37a99bd8fbba3b71e088848b6891e5e6bf62e55e
JohnNils Olson <johnnils@gmail.com>
2020-07-16 14:57:55 -0700

    add code to main to run program
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- NumbersToWords/Program.cs
+++ NumbersToWords/Program.cs
@@ -1,10 +1,17 @@
+using System;
+using NumbersToWords.Models;
+
 namespace NumbersToWords
 {
   public class NumbersToWordsProgram
   {
     public static void Main()
     {
-
+      Console.WriteLine("Input number to convert");
+      long input = long.Parse(Console.ReadLine());
+      NumberToConvert numObj = new NumberToConvert(input);
+      numObj.ConvertNumber();
+      Console.WriteLine(numObj.StringNumber);
     }
   }
 }
